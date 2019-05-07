a8842acbda70c93e944fbe748b8953bb08c08f6d
pavelnoen94 <38912521+pavelnoen94@users.noreply.github.com>
2019-05-07 16:41:29 +0200

    wrapped some loose code inside a function

--- js/themes.js
+++ js/themes.js
@@ -1,15 +1,17 @@
 // this part counts how many directories are between this file and parent directory
-let allHrefParts = document.location.href.split("/");
-let hrefParts = [];
+function numOfDirsToParent(){
+    let allHrefParts = document.location.href.split("/");
+    let hrefParts = [];
 
-for(i = 0; i < allHrefParts.length; i++){ // loop through the entire loop
-    if(allHrefParts[i] == "dot-slash-victoria"){ // When "dot-slash-victoria" (parent directory) is found...
-        for(j = 0; j < allHrefParts.length - i; j++){
-            hrefParts[j] = allHrefParts[j + i]; // Add rest of the array to the new array
+    for(i = 0; i < allHrefParts.length; i++){ // loop through the entire loop
+        if(allHrefParts[i] == "dot-slash-victoria"){ // When "dot-slash-victoria" (parent directory) is found...
+            for(j = 0; j < allHrefParts.length - i; j++){
+                hrefParts[j] = allHrefParts[j + i]; // Add rest of the array to the new array
+            }
         }
     }
+    return hrefParts.length - 2;
 }
-let numOfDirsToParent = hrefParts.length - 2;
 
 function setTheme(theme){
     // save settings to users browser.
@@ -19,7 +21,7 @@ function setTheme(theme){
     const STYLE_PREFIX = "<link id=\"theme-link\" rel=\"stylesheet\" type=\"text/css\" href=\"";
     const STYLE_SUFFIX = "/>";
     let insertion = STYLE_PREFIX;
-    for (i = 0; i < numOfDirsToParent; i++) {
+    for (i = 0; i < numOfDirsToParent(); i++) {
         insertion += "..\\";
     }
     insertion += PATH + theme + ".css\"" + STYLE_SUFFIX;
