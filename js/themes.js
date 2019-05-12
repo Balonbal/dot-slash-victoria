bb5d1a25117f7ea2b621225048bee52efeb361e2
pavelnoen94 <38912521+pavelnoen94@users.noreply.github.com>
2019-05-12 20:07:14 +0200

    Fixes a selector. Removed old theme logic

--- js/themes.js
+++ js/themes.js
@@ -1,30 +1,6 @@
-// this part counts how many directories are between this file and parent directory 
-function numOfDirsToParent() {
-  let allHrefParts = document.location.href.split('/');
-  let hrefParts = [];
-  for (i = 0; i < allHrefParts.length; i++) { // loop through the entire loop
-    // When "dot-slash-victoria" (parent directory) is found...
-    if (allHrefParts[i] == 'dot-slash-victoria') {
-      for (j = 0; j < allHrefParts.length - i; j++) {
-        hrefParts[j] = allHrefParts[j + i]; // Add rest of the array to the new array
-      }
-
-    }
-
-  }
-
-  return hrefParts.length - 2;
-}
-
 function setTheme(theme) {
   // save settings to users browser.
   window.localStorage.setItem('theme', theme);
-  /*
-  let pathToRoot = '';
-  for (i = 0; i < numOfDirsToParent(); i++) {
-    pathToRoot += '../';
-  }
-  */
 
   if (theme === 'dark') {
     $('#theme-link-dark').attr('rel', 'stylesheet');
@@ -34,7 +10,7 @@ function setTheme(theme) {
     $('#theme-link-light').attr('rel', 'stylesheet');
   }
 
-  $('.theme-selector').val(theme);
+  $('#theme-selector').val(theme);
 }
 
 // drop down menu change listener
