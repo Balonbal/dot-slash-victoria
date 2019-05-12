3dcf71a7eb8db013d19ea70bf3625657b4c029b0
pavelnoen94 <38912521+pavelnoen94@users.noreply.github.com>
2019-05-12 19:55:08 +0200

    made all themes load on startup. Offline functionality restored.

--- js/themes.js
+++ js/themes.js
@@ -1,31 +1,46 @@
-// this part counts how many directories are between this file and parent directory
-function numOfDirsToParent(){
-    let allHrefParts = document.location.href.split("/");
-    let hrefParts = [];
+// this part counts how many directories are between this file and parent directory 
+function numOfDirsToParent() {
+  let allHrefParts = document.location.href.split('/');
+  let hrefParts = [];
+  for (i = 0; i < allHrefParts.length; i++) { // loop through the entire loop
+    // When "dot-slash-victoria" (parent directory) is found...
+    if (allHrefParts[i] == 'dot-slash-victoria') {
+      for (j = 0; j < allHrefParts.length - i; j++) {
+        hrefParts[j] = allHrefParts[j + i]; // Add rest of the array to the new array
+      }
 
-    for(i = 0; i < allHrefParts.length; i++){ // loop through the entire loop
-        if(allHrefParts[i] == "dot-slash-victoria"){ // When "dot-slash-victoria" (parent directory) is found...
-            for(j = 0; j < allHrefParts.length - i; j++){
-                hrefParts[j] = allHrefParts[j + i]; // Add rest of the array to the new array
-            }
-        }
     }
-    return hrefParts.length - 2;
+
+  }
+
+  return hrefParts.length - 2;
 }
 
-function setTheme(theme){
-    // save settings to users browser.
-    window.localStorage.setItem("theme", theme);    
-    let pathToRoot = "";
-    for (i = 0; i < numOfDirsToParent(); i++){ pathToRoot += "../"; }
-        $("#theme-link").attr("href", pathToRoot + "css/themes/" + theme + ".css");
-    
-    document.getElementById("theme-selector").value = theme;
+function setTheme(theme) {
+  // save settings to users browser.
+  window.localStorage.setItem('theme', theme);
+  /*
+  let pathToRoot = '';
+  for (i = 0; i < numOfDirsToParent(); i++) {
+    pathToRoot += '../';
+  }
+  */
+
+  if (theme === 'dark') {
+    $('#theme-link-dark').attr('rel', 'stylesheet');
+    $('#theme-link-light').attr('rel', 'alternate stylesheet');
+  } else {
+    $('#theme-link-dark').attr('rel', 'alternate stylesheet');
+    $('#theme-link-light').attr('rel', 'stylesheet');
+  }
+
+  $('.theme-selector').val(theme);
 }
 
 // drop down menu change listener
-document.getElementById("theme-selector").addEventListener("change",function(){
-    if(document.getElementById("theme-selector")){
-        setTheme(document.getElementById("theme-selector").value);
+$('#theme-selector').change(function () {
+    console.log('theme change detected! ' + $('#theme-selector').val());
+    if ($('#theme-selector')) {
+      setTheme($('#theme-selector').val());
     }
-});
+  });
