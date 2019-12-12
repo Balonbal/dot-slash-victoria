d2ba04dc881f92af8a9748f012004514c28c2aba
Pavel Skipenes <pavelgs@stud.ntnu.no>
2019-12-12 11:01:45 +0100

    fixed theme and links to css

--- js/themes.js
+++ js/themes.js
@@ -1,4 +1,4 @@
-function setTheme(theme) {
+/* function setTheme(theme) {
   // save settings to users browser.
   window.localStorage.setItem('theme', theme);
 
@@ -19,3 +19,42 @@ $('#theme-selector').change(function () {
       setTheme($('#theme-selector').val());
     }
   });
+
+  */
+
+  
+ function Styler() {
+  this.SetTheme = function(theme) {
+    this.theme = theme;
+    window.localStorage.setItem("theme", theme);
+
+    if(theme == "dark"){
+      $(".darkStyleSheet").removeAttr("disabled")
+    }else{
+      $(".darkStyleSheet").attr("disabled", "disabled")
+    }
+  }
+}
+
+ function addTheme(theme) {
+	let text;
+	switch (theme) {
+		case "light": text = "Light"; break;
+		case "dark": text = "Dark"; break;
+	}
+	$("<a href='javascript:void(0)'></a>")
+		.addClass("dropdown-item")
+		.append($("<span>").addClass("t").text("theme_" + theme))
+		.on("click", () => {
+			if (!styler) return;
+			styler.SetTheme(theme);
+		}).appendTo($(".themeList"));
+}
+
+styler = new Styler();
+window.addEventListener("load", function() {
+  addTheme("light");
+  addTheme("dark");
+	let theme = window.localStorage.getItem("theme");
+	styler.SetTheme(theme || "light");
+});
