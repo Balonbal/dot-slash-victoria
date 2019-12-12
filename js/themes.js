8a84a3d586b0044a943ae8ae6182f5bef795c449
Pavel Skipenes <pavelgs@stud.ntnu.no>
2019-12-12 11:02:32 +0100

    forgot to save themes.js in last commit

--- js/themes.js
+++ js/themes.js
@@ -1,28 +1,3 @@
-/* function setTheme(theme) {
-  // save settings to users browser.
-  window.localStorage.setItem('theme', theme);
-
-  if (theme === 'dark') {
-    $('#theme-link-dark').attr('rel', 'stylesheet');
-    $('#theme-link-light').attr('rel', 'alternate stylesheet');
-  } else {
-    $('#theme-link-dark').attr('rel', 'alternate stylesheet');
-    $('#theme-link-light').attr('rel', 'stylesheet');
-  }
-
-  $('#theme-selector').val(theme);
-}
-
-// drop down menu change listener
-$('#theme-selector').change(function () {
-    if ($('#theme-selector')) {
-      setTheme($('#theme-selector').val());
-    }
-  });
-
-  */
-
-  
  function Styler() {
   this.SetTheme = function(theme) {
     this.theme = theme;
