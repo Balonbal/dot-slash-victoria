4a3dde5663501a007289fe4515802ac49c7561f6
Olav Bech Bråten <olavbb@hotmail.com>
2020-01-13 17:47:18 +0100

    Add example test

---
+++ __tests__/mainpage.js
@@ -0,0 +1,34 @@
+const puppeteer = require("puppeteer");
+
+const appBase = "http://localhost:4000";
+const routes = {
+	mainpage: appBase,
+};
+//Setup
+let browser, page
+beforeAll(async () => {
+	browser = await puppeteer.launch();
+	page = await browser.newPage();
+	await page.goto(routes.mainpage);
+});
+
+describe("mainpage:navigation", () => {
+	describe("Mainpage links are visible", () => {
+		const links = [ 
+			{ id: "uni_pButton", text: "uni_p" } 
+		];
+		for (let i in links) {
+			const id = links[i].id;
+			const expectedText = links[i].text;
+			test(`Link ${id} contains text "${expectedText}"`, async() => {
+				const link = await page.waitForSelector(`[data-testid='${id}']`);
+				const name = await link.evaluate(node => node.innerText);
+				expect(name.toLowerCase()).toContain("uni_p");
+			});
+		}
+	});
+});
+
+afterAll(() => {
+	if (!process.env.DEBUG) browser.close();
+});
