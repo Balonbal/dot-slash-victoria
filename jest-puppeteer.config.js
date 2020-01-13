6544172f968d7d7eda4f3b11d00b5b5741f0c90c
Olav Bech Bråten <olavbb@hotmail.com>
2020-01-13 19:51:37 +0100

    Disable sandboxing

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-	launch: { executablePath: "google-chrome-unstable" },
+	launch: { 
+		executablePath: "google-chrome-unstable",
+		args: [ "--no-sandbox" ]
+	},
 	server: {
 		command: "jekyll serve --trace",
 		port: "4000"
